bf07f0943240498de0f0ddea4be5fa1cc50d2cc4
Luoshuang246850 <58174173+Luoshuang246850@users.noreply.github.com>
2021-10-29 20:01:08 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,34 @@
 
 module.exports = {
     devServer: {
+<<<<<<< HEAD
+        proxy: {
+            '/back': {
+                target: 'http://172.17.49.29:8080/',
+				// pathRewrite:{'^/api':''}
+                changeOrigin: true
+            },
+			'/getCaptcha':{
+				target:'http://172.17.49.29:8080/',
+				changeOrigin:true
+			},
+			'/admin':{
+				target:'http://172.17.49.29:8080/',
+				changeOrigin:true
+			},
+			'/judgeCaptcha':{
+				target:'http://172.17.49.29:8080/',
+				changeOrigin:true
+			}
+        }
+			// proxyTable:{
+			// 	'/back':{
+			// 		target:'http://1.15.243.210:8080/',
+			// 		// changeOrigin:true
+			// 		pathRewrite:{'^/back':''}
+			// 	},
+			// }
+=======
         // proxy: {
         //     '/back': {
         //         target: 'http://1.15.243.210:8080/',
@@ -15,6 +43,7 @@ module.exports = {
 					pathRewrite:{'^/back':''}
 				},
 			}
+>>>>>>> 9c64dd2a89f38b9b94a98020bd26cdd25cd086ca
     },
 
 };
